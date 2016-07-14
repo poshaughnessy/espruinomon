e325f5bab8aaf68738253268b3da89f9e7356cf0
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-07-14 17:30:40 +0100

    Game logic

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var g;
 
 var img = require("BMPLoader").load(atob("Qk3AAAAAAAAAAD4AAAAoAAAAIAAAACAAAAABAAEAAAAAAIIAAAASCwAAEgsAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAPAAAAAIKgAAbLIAAMNMAACFKgABCEEAAwCFAA9SEQADBbkABkBmABlKkQAWIEEABACSAAoVAgBSEEIBoSAaBgpnEgCAAgoFFGpiAAA48AIAEGwAIBACAUBUoAAAQQkAAEgKwABAAIAA8ALgAOAB0ACAAGAAAAAAAA"));
 
+var STATUS = {
+  DEFAULT: 'DEFAULT',
+  DEFENDING: 'DEFENDING'
+};
+
+var playerMe = {
+  health: 100,
+  status: STATUS.DEFAULT
+};
+
+
 function lcdInit(){
   A5.write(0); // GND
   A7.write(1); // VCC
@@ -12,16 +23,7 @@ function lcdInit(){
   spi.setup({ sck:B1, mosi:B10 });
   // Initialise the LCD
   g = require("PCD8544").connect(spi,B13,B14,B15, function() {
-    // When it's initialised, clear it and write some text
-
-    g.clear();
-    //g.drawString('Hello World!',0,0);
-    
-    g.drawImage(img, 10, 10);
-    
-    // send the graphics to the display
-    g.flip();
-    
+    redraw();
   });
 }
 
@@ -46,47 +48,52 @@ function onInit(){
 
 onInit();
 
-var cmd = '';
-
 Serial1.on('data', function (data) { 
   console.log('data', JSON.stringify(data));
-  cmd += data;
-  var idx = cmd.indexOf("\r");
-  if (idx > -1) {
-    console.log('recieved:', cmd);
-    processData(cmd);
-    cmd='';
-  }
+  if (data === 'A') {
+    console.log('Been attacked!');
+    playerMe.health = playerMe.health - 10;
+    playerMe.status = STATUS.DEFAULT;
+    redraw();
+  }  
 });
 
-
-var playerMe = {
-  health: 100
-};
+function drawCharacter() {
+  console.log('Draw character');
+  g.drawImage(img, 10, 5);  
+}
 
 function drawHealthMeter() {
   var string = '';
-  for (var i=0; i < playerMe.health / 10; i++) {
+  for (var i=0; i <= playerMe.health / 10; i++) {
     console.log('string', string);
     string += '*';
   }
-  console.log('DRAW HEALTH METER');
+  console.log('Draw health');
+  g.drawString(string,10,40);
+}
+
+function redraw() {
   g.clear();
-  g.drawString(string,0,10);
+  drawCharacter();
+  drawHealthMeter();
   g.flip();
 }
 
-
+// Defend
 function button1(e){
-  console.log('button 1 pressed');
-  send('Hello');
-  drawHealthMeter();
+  console.log('Defend button pressed');
+  playerMe.status = STATUS.DEFENDING;
 }
 
 setWatch( button1, B3, { repeat: true, debounce : 50, edge: "rising" });
 
+// Attack
 function button2(e) {
-  console.log('button 2 pressed');
+  console.log('Attack button pressed');
+  playerMe.status = STATUS.DEFAULT;
+  send('A');
+  redraw();
 }
 
 setWatch(button2, B4, { repeat: true, debounce : 50, edge: "rising" });
@@ -94,8 +101,4 @@ setWatch(button2, B4, { repeat: true, debounce : 50, edge: "rising" });
 function send(data) {
   Serial1.println(data);
   console.log('Sent: ', data);
-}
-
-function processData(data){
-  console.log('processed:',data);
 }
